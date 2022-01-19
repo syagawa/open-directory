7149ade00283b7be01578b403f34c6a45320d030
syagawa <syagawapp@gmail.com>
2022-01-19 22:33:30 +0900

    Add index.js and yargs

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const { getOs } = require("./lib/detect.js");
+const { openDirectory } = require("./lib/open.js");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+const os = getOs();
+
+const run = async function(arg){
+  const dir = arg.dir || "./";
+  await openDirectory({ dir: dir, os: os })
+    .catch(function(err){
+      console.log(err);
+    });
+};
+
+run(argv);
